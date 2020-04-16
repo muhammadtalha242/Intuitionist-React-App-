abafef04cd6bc10647d1a1a6c93fd4839c7cc548
Talha <mabbas.bese16seecs@seecs.edu.pk>
2020-04-16 06:27:59 +0500

    Functon to detect and plant commissioned or ret in given period

--- backend/routes/SDDP/Modification Sheets/InputSheetsModification.js
+++ backend/routes/SDDP/Modification Sheets/InputSheetsModification.js
@@ -1,51 +1,73 @@
 
 
 class InputModificationSheet {
-    constructor(output){
+    constructor(output) {
         this.output = output
     }
 
 
     getData() {
-        console.log("Giving data away", )
-
-        
+        console.log("Giving data away")
     }
-    extractPlants(powerplants, type) {
-        const dateArray = Object.keys(Object.values(this.output)[0])
+
+    extractPlants(powerplants, fuel_category) {
+        const dateArray = Object.keys(Object.values(this.output)[0]).reverse()
 
         const sepratedPowerPlants = []
         powerplants.forEach(plant => {
 
-            if (plant.fuel_category === type) {
-                console.log("plant in thermel")
-                
+            if (plant.fuel_category === fuel_category) {
+                console.log(`plant in ${fuel_category}`)
 
-                dateArray.forEach(date=>{
+                var isCommissioned = true
+                var isRetired = false
+                dateArray.forEach(date => {
                     const currentDate = new Date(date)
                     const plant_cod = new Date(plant.cod)
-                    const cod_difference =((currentDate.getFullYear() - plant_cod.getFullYear()) * 12) + (currentDate.getMonth() - plant_cod.getMonth());
-                    const end_difference =  plant.end_year - currentDate.getFullYear() ;
-                    if(cod_difference >0 && end_difference > 0 ) {
-                        console.log('Plant not included ', plant.plant_name)
-                        console.log('cod_difference: ',cod_difference,currentDate, plant_cod)
-                        console.log('end_difference: ',end_difference)
-                    }
-                    else {
-                        console.log('Plant included ',plant.plant_name)
-                        console.log('cod_difference: ',cod_difference,currentDate, plant_cod)
-                        console.log('end_difference: ',end_difference)
-                        sepratedPowerPlants.push(plant)
-                    }                   
+                    const cod_difference = ((currentDate.getFullYear() - plant_cod.getFullYear()) * 12) + (currentDate.getMonth() - plant_cod.getMonth());
+                    const end_difference = plant.end_year - currentDate.getFullYear();
+                   
+                        if (cod_difference < 0) {
+                            isCommissioned = false
+                            console.log(`Plant not cod  ${plant.plant_name}`)
 
+                        }
+                        else if (cod_difference >= 0 && !isCommissioned) {
+                            isCommissioned = true
+                            console.log(`Plant cod in ${plant.plant_name}`)
+                            console.log(`date: ${date}`)
+                            plant['change'] = 'com'
+                            plant['change_date'] = date
+                            if (!sepratedPowerPlants.includes(plant)) {
+                                console.log(`plant going in ${plant.plant_name}`)
+                                sepratedPowerPlants.push(plant)
+                                
+                            }
+                        }
+                        else {
+                            console.log(`Plant HERE ${plant.plant_name}`)
+
+                        }
+                        if (end_difference <= 0) {
+
+                            console.log(`Plant retired  ${plant.plant_name}, date:  ${date}`)
+                            plant['change'] = 'ret'
+                            plant['change_date'] = date
+                            if (!sepratedPowerPlants.includes(plant)) {
+                                console.log(`plant going in ${plant.plant_name}`)
+                                sepratedPowerPlants.push(plant)
+                                
+                            }
+                        }
+                        
                 })
-    
+
 
 
             }
 
         })
-        
+
 
         return sepratedPowerPlants
     }
@@ -60,30 +82,30 @@ class InputModificationSheet {
         })
         return tipo
     }
-    getCvariables  (powerplant)  {
+    getCvariables(powerplant) {
         const cvarArray = ["VOM_Local", "VOM_Foreign", "WaterCharges", "LimeStoneCharges", "AshDisposalCost", "VariableRate", "VariableCostJetty"]
-    
+
         var sum = 0
         cvarArray.forEach(cvar => {
-    
+
             const plantsArray = Object.values(this.output[cvar])[1][1]
-            
+
             plantsArray.forEach(plant => {
-    
-    
+
+
                 if (plant.name === powerplant.plant_name) {
                     var index = plant.index
-                    if (typeof (plant.index) === 'object' && (plant.index !== null)) {
+                    if (fuel_categoryof(plant.index) === 'object' && (plant.index !== null)) {
                         index = index[0]["rate"]
                     }
                     sum = sum + index
-    
+
                 }
             })
-    
+
         })
         return sum
     }
-    
+
 }
 module.exports = InputModificationSheet
