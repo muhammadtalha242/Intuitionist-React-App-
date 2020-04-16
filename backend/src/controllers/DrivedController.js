718cecf25d69bf82878f850ac430f1d0142f1cb1
Talha <mabbas.bese16seecs@seecs.edu.pk>
2020-04-16 17:17:21 +0500

    drivedController

--- backend/src/controllers/DrivedController.js
+++ backend/src/controllers/DrivedController.js
@@ -13,44 +13,42 @@ drivedController.delete("/:id", remove);
 const powerplants = db.getModel("powerplants");
 const techparams = db.getModel("techparams");
 const ecoparams = db.getModel("economicparams");
-powerplants.belongsTo(ecoparams,{foreignKey: 'economic_parameters_id'})
-ecoparams.hasOne(powerplants,{foreignKey: 'economic_parameters_id'})
-powerplants.belongsTo(techparams,{foreignKey: 'technical_parameter_id'})
-techparams.hasOne(powerplants,{foreignKey: 'technical_parameter_id'})
+powerplants.belongsTo(ecoparams, { foreignKey: 'economic_parameters_id' })
+ecoparams.hasOne(powerplants, { foreignKey: 'economic_parameters_id' })
+powerplants.belongsTo(techparams, { foreignKey: 'technical_parameter_id' })
+techparams.hasOne(powerplants, { foreignKey: 'technical_parameter_id' })
 
 logger.info("query.page");
 
 async function all(req, res) {
-    var page=1;
-    logger.info("query.page",req.query.page);
+    var page = 1;
+    logger.info("query.page", req.query.page);
     if (req.query.page) {
-      page = parseInt(req.query.page);
-     
+        page = parseInt(req.query.page);
+
     }
     var limit = maxItems;
     var offset = (page - 1) * maxItems;
-    logger.info("req.drivedUrl",req.drivedUrl);
+    logger.info("req.drivedUrl", req.drivedUrl);
 
     var collection = await powerplants.findAll({
         include: [
             {
-             
-              model: ecoparams
+                model: ecoparams
             },
-          
             {
                 model: techparams,
-             
             }
-          ]
-    },{ limit: limit, offset: offset })
-    if (collection.length<1) {
-        logger.fail(`404 /${modelName}`,collection.length);
-    }else{
+        ]
+        , limit: limit, offset: offset
+    })
+    if (collection.length < 1) {
+        logger.fail(`404 /${modelName}`, collection.length);
+    } else {
 
         logger.success(
-          `200 /${collection}/all`,
-          `[${collection.length}] Item(s)`
+            `200 /${collection}/all`,
+            `[${collection.length}] Item(s)`
         );
     }
     return res.status(200).send(collection);
@@ -58,10 +56,19 @@ async function all(req, res) {
 
 async function byId(req, res) {
     var id = req.params.id;
-    var modelName = req.drivedUrl.replace("/", "");    
-    var response = await db.getModel(modelName).findByPk(id);
-    if(response===null){
-        logger.fail(`404 /${modelName}/byId:${id}`,response);
+    var modelName = req.drivedUrl.replace("/", "");
+    var response = await powerplants.findByPk(id)({
+        include: [
+            {
+                model: ecoparams
+            },
+            {
+                model: techparams,
+            }
+        ]
+    });
+    if (response === null) {
+        logger.fail(`404 /${modelName}/byId:${id}`, response);
         return res.status(404).send(response);
     }
     logger.success(`200 /${modelName}/byId:${id}`, response);
@@ -69,22 +76,21 @@ async function byId(req, res) {
 }
 
 async function add(req, res) {
-    try{
+    try {
         var newModel = req.body;
         var modelName = req.drivedUrl.replace("/", "");
         await db.getModel(modelName).create(newModel);
         logger.success(`200 /${modelName}/add`, newModel);
         return res.status(201).send(true);
     }
-    catch(error){
+    catch (error) {
         logger.fail(`400 /${modelName}/add`, error.errors[0].message);
         return res.status(400).send(fail);
     }
 }
 
 async function update(req, res) {
-    try
-    {
+    try {
         var id = req.params.id;
         var updatedModel = req.body;
         var modelName = req.drivedUrl.replace("/", "");
@@ -93,7 +99,7 @@ async function update(req, res) {
         // console.log("toUpdate: ",toUpdate);
         toUpdate.update(updatedModel);
         logger.success(`200 /${modelName}/update:${id}`, toUpdate);
-        return res.status(200).send(toUpdate); 
+        return res.status(200).send(toUpdate);
     } catch (error) {
         logger.fail(`400 /${modelName}/update:${id}`, error);
         return res.status(400).send(false);
@@ -102,15 +108,15 @@ async function update(req, res) {
 
 async function remove(req, res) {
     try {
-      var id = req.params.id;
-      var modelName = req.drivedUrl.replace("/", "");
-      var toRemove = await db.getModel(modelName).findByPk(id);
-      toRemove.destroy();
-      logger.success(`200 /${modelName}/remove:${id}`, toRemove);
-      return res.status(202).send(true);
+        var id = req.params.id;
+        var modelName = req.drivedUrl.replace("/", "");
+        var toRemove = await db.getModel(modelName).findByPk(id);
+        toRemove.destroy();
+        logger.success(`200 /${modelName}/remove:${id}`, toRemove);
+        return res.status(202).send(true);
     } catch (error) {
-      logger.fail(`400 /${modelName}/remove:${id}`, error);
-      return res.status(400).send(false);
+        logger.fail(`400 /${modelName}/remove:${id}`, error);
+        return res.status(400).send(false);
     }
 }
 
