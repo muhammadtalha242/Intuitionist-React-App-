3fcd9fdb1798336a8dbcd1be3fade1e2997a0855
kafaite zahra <kafait.e.zahra@gmail.com>
2020-06-12 23:41:45 +0500

    heroku host

--- routes/DataBaseModule/config.js
+++ routes/DataBaseModule/config.js
@@ -1,7 +1,7 @@
-// const mysql = require('mysql');
+const mysql = require('mysql');
 const Sequelize = require('sequelize')
 module.exports = new Sequelize("heroku_baa5a0ae220b548", "b7fa20857068cd", "be8f54bf", {
-  host: "localhost",
+  host: "us-cdbr-east-05.cleardb.net",
   dialect: "mysql",
   operatorsAliases: false,
 
@@ -13,3 +13,17 @@ module.exports = new Sequelize("heroku_baa5a0ae220b548", "b7fa20857068cd", "be8f
     idle: 100000,
   },
 });
+// const Sequelize = require('sequelize')
+// module.exports = new Sequelize("intuitionist", "root", "root", {
+//   host: "localhost",
+//   dialect: "mysql",
+//   operatorsAliases: false,
+
+//   pool: {
+//     max: 36,
+//     min: 0,
+//     // require: 3,
+//     acquire: 300000,
+//     idle: 100000,
+//   },
+// });
