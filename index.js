e9db4f58014a13fd1e2dc94a50384f200d74206e
Asad Awan <asad.awan.se@gmail.com>
2020-06-14 15:33:12 +0500

    production ready

--- index.js
+++ index.js
@@ -42,8 +42,12 @@ app.use('/update', updateDatabase)
 if(process.env.NODE_ENV === 'production'){
 	app.use(express.static('frontend/build'));
 
-	app.get('*', (req,res)=>{
-		res.sendFile(path.join(__dirname, 'frontend','build','index.html'))
+	app.use(express.static(path.join(__dirname, '../frontend/build')));
+	app.get('/', function(req, res) {
+		res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
+	});
+	app.get('/*', function(req, res) {
+		res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'))
 	})
 }
 
